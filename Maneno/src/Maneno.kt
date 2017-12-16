e9c47ac4bd7258a830f63aeb32c058004c71a21c
tamzi <tamrefrank@gmail.com>
2017-12-16 06:19:55 +0300

    Added basic explanation on variables

---
+++ Maneno/src/Maneno.kt
@@ -0,0 +1,30 @@
+fun main(args :Array<String>){
+    println("YOLO \n I am a big Bay with a smile on my face\n twinkle twinkle little star\n How i wonder what you are!")
+
+    /* fun main is where your application is going to execute. Its your function.*/
+
+    /*VARIABLES
+    * There are 2 types of variables: 1.Mutable and immutable variables
+    * */
+
+    /*MUTABLE VARIABLES:
+    * var is for changing variables.
+    *
+    * */
+
+
+    /*IMMUTABLE VARIABLES:
+    * referred to as read only variable or unchangable variables...
+    * In below example your name will not change.
+     * We mostly use val
+    *
+    * */
+
+    var myAge = 55
+    val name = "TamreFrank"
+
+    /*Kotlin uses type inference meaning if we put a string, Kotin knows its a string.*/
+
+
+
+}
