2ceaed667debf76823436f0505c1a3d62921c4cc
tamzi <tamrefrank@gmail.com>
2017-12-16 07:06:22 +0300

    Added ranges and Loops

---
+++ Maneno/src/Loops/RangesAndLoops.kt
@@ -0,0 +1,15 @@
+package Loops
+/*
+
+fun main(args: Array<String>) {
+    for(a: Int in 1..100){
+        println(a)
+    }
+*/
+
+fun main(args:Array<String>){
+    for (a: Int in 1..100){
+        println(a)
+    }
+
+}
